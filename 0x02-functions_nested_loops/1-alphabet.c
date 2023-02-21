b45977cb0d209b5e8d9a0c8caf022ac48168743e
lensofadhia <worldie2021.com>
2023-02-21 09:25:30 -0800

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
        Prototype: void print_alphabet_x10(void);
        You can only use _putchar twice in your code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
+ * main - Prints the alphabet in lowercase follwed by a new line.
+ *
+ * Return: Always 0.
 */
+int main(void)
+{
+	void print_alphabet_x10(void);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
