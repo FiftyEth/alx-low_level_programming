d10165be7af3b43a56dc9ce0959821e269966622
lensofadhia <worldie2021.com>
2023-02-21 11:46:10 -0800

    Updating header with prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,7 @@
-void print_alphabet(void);
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void print_alphabet(void);
+int _putchar(char c);
+
+#endif
