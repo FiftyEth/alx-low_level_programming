dadfbab8c91fd788ac74508b3f68283b090c57f1
lensofadhia <worldie2021.com>
2023-02-27 09:19:23 -0800

    Prototype: char *_strcpy(char *dest, char *src);
    Write a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
    
        Return value: the pointer to dest
    
    FYI: The standard library provides a similar function: strcpy. Run man strcpy to learn m

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,16 @@
  *
  * Return: pointer to dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
+	int i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 int _atoi(char *s);
+void randomPasswordGeneration(int N);
