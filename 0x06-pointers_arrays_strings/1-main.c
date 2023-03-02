89d2f65866bbb7af85ae56e890f19f57d07deea8
lensofadhia <worldie2021.com>
2023-03-02 09:38:35 -0800

    Adding 2-main.c

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - Check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -14,7 +14,11 @@ int main(void)
 
 	printf("%s\n", s1);
 	printf("%s", s2);
-	ptr = _strcat(s1, s2);
+	ptr = _strncat(s1, s2, 1);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	printf("%s\n", ptr);
+	ptr = _strncat(s1, s2, 1024);
 	printf("%s", s1);
 	printf("%s", s2);
 	printf("%s", ptr);
