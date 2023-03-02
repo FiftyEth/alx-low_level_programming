95525d9ce4cd8a5f9f4be111afb6f4028abaf426
lensofadhia <worldie2021.com>
2023-03-02 12:15:37 -0800

    Write a function that encodes a string into 1337.
        Letters a and A should be replaced by 4
        Letters e and E should be replaced by 3
        Letters o and O should be replaced by 0
        Letters t and T should be replaced by 7
        Letters l and L should be replaced by 1
        Prototype: char *leet(char *);
        You can only use one if in your code
        You can only use two loops in your code
        You are not allowed to use switch
        You are not allowed to use any ternary operation

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
-
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i;
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (str[indx1])
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (str[indx1] == leet[indx2] ||
+			    str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
+
+		indx1++;
 	}
 
-	return (s);
+	return (str);
 }
