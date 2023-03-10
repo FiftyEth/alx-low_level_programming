b5d27c521b604ecef1cd9a4344daf696e20ab68a
lensofadhia <worldie2021.com>
2023-03-10 10:36:52 -0800

    Adding header file

--- 0x09-static_libraries/0x0A-argc_argv/main.h
+++ 0x09-static_libraries/0x0A-argc_argv/main.h
@@ -1,13 +1,6 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int main(int argc, char *argv[]);
+
+#endif
