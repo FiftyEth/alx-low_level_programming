6f64285b5a2d9e0d3d5f9d386d0e26e75d252632
lensofadhia <worldie2021.com>
2023-03-10 10:39:49 -0800

    Write a program that prints its name, followed by a new line.
        If you rename the program, it will print the new name, without having to compile it again
        You should not remove the path before the name of the program

--- 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always o.
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
